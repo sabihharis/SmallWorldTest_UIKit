65e6ae6b714d77a67e6f16cee02fd39f02d4f761
Sabih Haris <sabih.haris@avanzasolutions.com>
2023-12-12 14:16:04 +0500

    Filter by name done

--- SmallWorldTest_UIKit/View/Movies/MoviesViewController.swift
+++ SmallWorldTest_UIKit/View/Movies/MoviesViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class MoviesViewController: UIViewController {
     
     // MARK: - Outlets
-    
+    @IBOutlet weak var searchBar : UISearchBar!
     @IBOutlet weak var moviesTable : UITableView! {
         didSet {
             moviesTable.delegate = self
@@ -24,10 +24,13 @@ class MoviesViewController: UIViewController {
     
     private var moviesCell = "MoviesTableViewCell"
     private var navigationTitle = "Movies"
+    var searchActive : Bool = false
+
     
     var moviesViewModel : MoviesViewModel? = MoviesViewModel()
-    var moviesTableData : MovieResponse? = nil
-    
+    var moviesTableData : [MovieResult]? = nil
+    var moviesFilteredTableData : [MovieResult]? = nil
+
     
     // MARK: - View Life Cycle
     
@@ -36,6 +39,8 @@ class MoviesViewController: UIViewController {
         
         
         getMoviesData()
+        searchBar.delegate = self
+
         
     }
     
@@ -43,6 +48,7 @@ class MoviesViewController: UIViewController {
         super.viewWillAppear(animated)
         
         setupNavigationItem()
+        
     }
     
     
@@ -60,8 +66,9 @@ class MoviesViewController: UIViewController {
     func getMoviesData() {
         moviesViewModel?.getMovies { [weak self] movieResponse in
             if(movieResponse != nil) {
-                self?.moviesTableData = movieResponse
-                
+                self?.moviesTableData = movieResponse?.results
+                self?.moviesFilteredTableData = movieResponse?.results
+
                 DispatchQueue.main.async {
                     self?.moviesTable.reloadData()
                 }
@@ -72,17 +79,26 @@ class MoviesViewController: UIViewController {
     
 }
 
-// MARK: - Extensions
+// MARK: - Extensions - TableView
 
 extension MoviesViewController : UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return moviesTableData?.results?.count ?? 0
+        if(searchActive){
+            return self.moviesFilteredTableData?.count ?? 0
+        }
+        return self.moviesTableData?.count ?? 0
+
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: moviesCell, for: indexPath) as! MoviesTableViewCell
-        cell.bindData(data: moviesTableData?.results?[indexPath.row])
+
+        if(searchActive){
+            cell.bindData(data: moviesFilteredTableData?[indexPath.row])
+        } else {
+            cell.bindData(data: moviesTableData?[indexPath.row])
+        }
         cell.selectionStyle = .none
         return cell
     }
@@ -93,8 +109,77 @@ extension MoviesViewController : UITableViewDataSource {
 extension MoviesViewController : UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailViewController : MovieDetailViewController = MovieDetailViewController.initWithNib()
-        detailViewController.movieId = moviesTableData?.results?[indexPath.row].id ?? 0
-        detailViewController.navigationTitle = moviesTableData?.results?[indexPath.row].originalTitle ?? ""
+        detailViewController.movieId = moviesTableData?[indexPath.row].id ?? 0
+        detailViewController.navigationTitle = moviesTableData?[indexPath.row].originalTitle ?? ""
         self.navigationController?.pushViewController(detailViewController, animated: true)
+    }
+}
+
+// MARK: - Extensions - SearchBar
+
+extension MoviesViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+        searchBar.resignFirstResponder()
+        searchBar.showsCancelButton = false
+        searchBar.text = nil
+        performfilter(text: nil)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if searchBar.text == ""{
+            performfilter(text: nil)
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if !searchText.isEmpty {
+            performfilter(text: searchText)
+            return
+        }
+        performfilter(text: nil)
+    }
+    
+    private func performfilter(text:String?) {
+        let searchText = text
+        if let data = searchText {
+            var searched_transactions = [MovieResult]()
+            searched_transactions = (self.moviesTableData?.filter({( transaction : MovieResult) -> Bool in
+                if ((transaction.originalTitle?.lowercased().contains(data.lowercased())) ?? false) {
+                    return true
+                }
+                return false
+            }))!
+            self.searchActive = true
+            self.moviesFilteredTableData = searched_transactions
+        }
+        else{
+            resetSearch()
+        }
+        self.moviesTable.reloadData()
+    }
+    
+    func resetSearch() {
+        if #available(iOS 13.0, *) {
+            self.searchBar.searchTextField.text = ""
+        } else {
+            self.searchBar.text = ""
+        }
+        self.searchBar.showsCancelButton = false
+        self.searchActive = false
+        self.moviesFilteredTableData?.removeAll()
+        self.moviesFilteredTableData = nil
+        self.moviesTable.reloadData()
     }
 }
